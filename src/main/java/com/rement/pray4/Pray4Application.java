b395596c31c22d74605cb5890f2b3ae63299c487
Andrei Kaliachka <akaliachka@gmail.com>
2019-02-10 22:34:42 +0300

    init commit

---
+++ src/main/java/com/rement/pray4/Pray4Application.java
@@ -0,0 +1,14 @@
+package com.rement.pray4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pray4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pray4Application.class, args);
+	}
+
+}
+
